14b7a58a9192ef5178624a1366f01385be169436
arnachap <arnaud.chapeleur@gmail.com>
2019-06-26 10:48:28 +0200

    Bowties modals

--- resources/views/pages/collection.blade.php
+++ resources/views/pages/collection.blade.php
@@ -18,7 +18,7 @@ Notre collection
                 @foreach ($collection->bowties as $index => $bowtie)
                     @if($index < 4)
                         <div class="col-3">
-                            <figure class="bowtie">
+                            <figure class="bowtie" data-toggle="modal" data-target="#modal{{ $bowtie->id }}">
                                 <img src="/storage/bowties/{{ $bowtie->photo }}" alt="">
                                 <figcaption>
                                     <h2>{{ $bowtie->name }}</h2>
@@ -26,6 +26,41 @@ Notre collection
                             </figure>
                         </div>
                     @endif
+
+                    <div class="modal fade" id="modal{{ $bowtie->id }}" tabindex="-1" role="dialog" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+
+                                <div class="modal-body">
+                                    <h5 class="section-title">{{ $bowtie->name }}</h5>
+                                
+                                    <img src="/storage/bowties/{{ $bowtie->photo }}" class="img-fluid" alt="">
+
+                                    <br>
+
+                                    <p class="text-center text-muted">{{ $bowtie->description }}</p>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <div class="row w-100">
+                                        <div class="col-6">
+                                            @if(!is_null($bowtie->price))
+                                                <p class="text-left text-success mt-1">Prix : {{ $bowtie->price }}€</p>
+                                            @endif
+                                        </div>
+
+                                        <div class="col-6">
+                                            @if($bowtie->available)
+                                                <button type="button" class="btn btn-success d-block ml-auto">Ajouter au panier</button>
+                                            @else
+                                                <button type="button" class="btn btn-secondary d-block ml-auto" disabled>Indisponible</button>
+                                            @endif
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 @endforeach
             </div>
 
