866ae09a78924d8de6e1e73d9de42a856bfb3d34
arnachap <arnaud.chapeleur@gmail.com>
2019-08-14 13:17:33 +0200

    fix collection style

--- resources/views/pages/collection.blade.php
+++ resources/views/pages/collection.blade.php
@@ -70,7 +70,7 @@ Notre collection
                 <div class="row m-0 p-3">
                     @foreach ($collection->bowties as $index => $bowtie)
                         @if($index >= 4)
-                            <div class="col-12 col-md-3 p-0">
+                            <div class="col-12 col-md-3">
                                 <figure class="bowtie" data-toggle="modal" data-target="#modal{{ $bowtie->id }}">
                                     <img src="/storage/bowties/{{ $bowtie->photo }}" alt="" class="img-fluid">
                                     <figcaption>
