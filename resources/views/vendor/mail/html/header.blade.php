2faed2e7111fc41082415547b9c874398bb226be
arnachap <arnaud.chapeleur@gmail.com>
2019-08-20 13:19:00 +0200

    Change mail's footer & header

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="header">
+        <a href="{{ $url }}">
+            <img src="https://moon-noeudpap.fr/img/logo/cat.png" style="height: 100px;" alt="">
+        </a>
+    </td>
+</tr>
